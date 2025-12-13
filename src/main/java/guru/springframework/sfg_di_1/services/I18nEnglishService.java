dccb2cfe59fbc734799d578569cf054711d35515
Tom Thompson <tomthompsonrph2@gmail.com>
2025-12-13 01:16:08 -0700

    Added profile functionality to Greeting services for EN and ES Files Added: I18nController.java I18nEnglishService.java I18nSpanishService.java
    Added a default profile into the English service

--- src/main/java/guru/springframework/sfg_di_1/services/I18nEnglishService.java
+++ src/main/java/guru/springframework/sfg_di_1/services/I18nEnglishService.java
@@ -3,7 +3,7 @@ package guru.springframework.sfg_di_1.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN", "default"})
 @Service("i18nService")
 public class I18nEnglishService implements GreetingService{
 
